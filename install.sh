d4492aa35c2ff881dda38c6fbea1a9cda13931ec
Torrance Yang <torrance.yang@gmail.com>
2018-04-04 20:58:21 -0400

    Finished creating and performing sentiment analysis

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+apt update
+apt --yes install python python-dev python3 python3-dev
+easy_install pip
+pip install --ignore-installed google-cloud
+pip install --upgrade google-cloud-language
+pip install --upgrade nltk
+mkdir -p /usr/share/nltk_data
+python -c "import nltk; nltk.download('vader_lexicon', download_dir='/home/nltk_data/')"
--- local/setup_new_table.py
+++ local/setup_new_table.py
@@ -18,6 +18,7 @@ from google.cloud import bigquery
 from google.cloud.bigquery import SchemaField
 from google.cloud.bigquery import table
 
+
 # Create a new Google BigQuery client using Google Cloud Platform project
 # defaults.
 bigquery_client = bigquery.Client()
@@ -30,10 +31,10 @@ dataset_ref = bigquery_client.dataset(dataset_id)
 dataset = bigquery.Dataset(dataset_ref)
 
 # Create the new BigQuery dataset.
-dataset = bigquery_client.create_dataset(dataset)
+#dataset = bigquery_client.create_dataset(dataset)
 
 # In the new BigQuery dataset, create a new table.
-table_ref = dataset.table('merged_tables')
+table_ref = dataset.table('merged_tables_timestamped')
 # The table needs a schema before it can be created and accept data.
 # Create an ordered list of the columns using SchemaField objects.
 SCHEMA = []
@@ -43,6 +44,7 @@ SCHEMA.append(SchemaField('score', 'integer'))
 SCHEMA.append(SchemaField('gilded', 'integer'))
 SCHEMA.append(SchemaField('controversiality', 'integer'))
 SCHEMA.append(SchemaField('subreddit', 'string'))
+SCHEMA.append(SchemaField('created_utc', 'string'))
 
 # Assign the schema to the table and create the table in BigQuery.
 table = bigquery.Table(table_ref, schema=SCHEMA)
@@ -51,7 +53,7 @@ table = bigquery_client.create_table(table)
 # Set up a query in Standard SQL.
 # The query selects the fields of interest.
 QUERY = """
-SELECT body, author, score, gilded, controversiality, subreddit
+SELECT body, author, score, gilded, controversiality, subreddit, created_utc
 FROM `fh-bigquery.reddit_comments.2015_10`
 WHERE subreddit = 'The_Donald' OR subreddit = 'politics' OR subreddit = 'SandersForPresident' OR subreddit = 'hillaryclinton' OR subreddit = 'worldnews'
 AND body IS NOT NULL
@@ -61,7 +63,7 @@ AND gilded IS NOT NULL
 AND controversiality IS NOT NULL
 AND subreddit IS NOT NULL
 UNION ALL SELECT 
-body, author, score, gilded, controversiality, subreddit
+body, author, score, gilded, controversiality, subreddit, created_utc
 FROM `fh-bigquery.reddit_comments.2016_10`
 WHERE subreddit = 'The_Donald' OR subreddit = 'politics' OR subreddit = 'SandersForPresident' OR subreddit = 'hillaryclinton' OR subreddit = 'worldnews'
 AND body IS NOT NULL
@@ -71,7 +73,7 @@ AND gilded IS NOT NULL
 AND controversiality IS NOT NULL
 AND subreddit IS NOT NULL
 UNION ALL SELECT 
-body, author, score, gilded, controversiality, subreddit
+body, author, score, gilded, controversiality, subreddit, created_utc
 FROM `fh-bigquery.reddit_comments.2017_10`
 WHERE subreddit = 'The_Donald' OR subreddit = 'politics' OR subreddit = 'SandersForPresident' OR subreddit = 'hillaryclinton' OR subreddit = 'worldnews'
 AND body IS NOT NULL
